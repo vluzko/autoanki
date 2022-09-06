7209c8dbe293d45def627c6787b3462d2b5745ed
Vincent Luczkow <vincent.luczkow@gmail.com>
2022-09-05 22:30:06 -0700

    Note update test

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -9,25 +9,41 @@ ANKI_REF = ANKI_PATH / 'User 1' / 'collection.bak'
 
 
 def test_coll():
-    coll = backend.get_collection('User 1', base_path=ANKI_PATH)
+    coll = backend.get_collection('User 1', anki_path=ANKI_PATH)
 
 
 def test_get_deck():
-    coll = backend.get_collection('User 1', base_path=ANKI_PATH)
+    coll = backend.get_collection('User 1', anki_path=ANKI_PATH)
     deck = backend.get_deck(coll, 'Default')
 
 
 def test_get_note_type():
-    coll = backend.get_collection('User 1', base_path=ANKI_PATH)
+    coll = backend.get_collection('User 1', anki_path=ANKI_PATH)
     note_type = backend.get_note_type(coll, 'Basic')
 
 
 def test_add_note():
-    coll = backend.get_collection('User 1', base_path=ANKI_PATH)
+    coll = backend.get_collection('User 1', anki_path=ANKI_PATH)
     new_note = backend.add_note(coll, 'Default', 'Basic')
     all_notes = coll.find_notes('')
     assert len(all_notes) == 1
 
+
+def test_update_note():
+    coll = backend.get_collection('User 1', anki_path=ANKI_PATH)
+    new_note = backend.add_note(coll, 'Default', 'Basic')
+    del coll
+    coll = backend.get_collection('User 1', anki_path=ANKI_PATH)
+    all_notes = coll.find_notes('')
+    note_id = all_notes[0]
+    note = coll.get_note(note_id)
+    note.fields[0] = 'test'
+    coll.update_note(note)
+    coll.db.commit()
+    del coll
+    coll = backend.get_collection('User 1', anki_path=ANKI_PATH)
+    note = coll.get_note(note_id)
+    assert note.fields[0] == 'test'
 
 
 @fixture(autouse=True)
